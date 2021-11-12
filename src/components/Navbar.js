fdab2853d1603e4944b97a24699f94e86d86dd59
Kushagra-Pathak <53998685+Kushagra-Pathak@users.noreply.github.com>
2021-11-12 20:58:31 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,19 @@
 import React from "react";
-
+import "./Header.css";
 function Navbar() {
-  return <h2>Navbar</h2>;
+  return (
+    <div id="navbar">
+      <div id="logo">Mindspace</div>
+      <div id="elements">
+        <div class="element">Doctors</div>
+        <div class="element">Book Appointment</div>
+        <div class="element">Blogs</div>
+        <div class="element">Daily Tips</div>
+        <div id="login">
+          <button id="login-button">Login</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Navbar;
