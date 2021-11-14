1d90d61324e326ded4bc6dc82acd3958c3a45e0b
Kushagra-Pathak <53998685+Kushagra-Pathak@users.noreply.github.com>
2021-11-15 02:14:14 +0530

    Create Doctor.js

---
+++ src/containers/Doctor.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Doctor_Card from "../components/Doctor_Card.js";
+function Doctor() {
+  return (
+    <div>
+      <Doctor_Card />
+      <Doctor_Card />
+    </div>
+  );
+}
+export default Doctor;
