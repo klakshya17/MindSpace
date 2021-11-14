029f130925cf23d6c5151754860b40ee46dd006b
Kushagra-Pathak <53998685+Kushagra-Pathak@users.noreply.github.com>
2021-11-15 02:23:28 +0530

    Update Doctor_Card.js

--- src/components/Doctor_Card.js
+++ src/components/Doctor_Card.js
@@ -1,25 +1,22 @@
-.Footer {
-  height: 150px;
-  display: grid;
-  grid-template-columns: 1fr 1fr auto;
-  background-color: #282c34;
-  color: white;
-  margin: 0px;
+import React from "react";
+import "./Doctor_Card.css";
+function Doctor_Card() {
+  return (
+    <div id="doccard">
+      <img
+        src="https://img.freepik.com/free-vector/doctor-character-background_1270-84.jpg?size=338&ext=jpg"
+        alt=""
+      />
+      <h3 className="cardEl">Dr. ABC</h3>
+      <h5 className="cardEl">MBBS</h5>
+      <p className="cardEl">
+        In publishing and graphic design, Lorem ipsum is a placeholder text
+        commonly used to demonstrate the visual form of a document or a typeface
+        without relying on meaningful content. Lorem ipsum may be used as a
+        placeholder before final copy is available.
+      </p>
+      <button className="cardEl"> Book Appointment</button>
+    </div>
+  );
 }
-.Footer img {
-  height: 150px;
-}
-.Footer h3 {
-  text-align: right;
-  justify-content: right;
-  align-items: right;
-  margin: auto;
-}
-ul {
-  justify-content: center;
-  align-items: center;
-  margin: auto;
-}
-li {
-  list-style-type: none;
-}
+export default Doctor_Card;
