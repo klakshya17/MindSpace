f4a3fefd8d74f8738cdeeed1d168983a248af99e
Kushagra-Pathak <53998685+Kushagra-Pathak@users.noreply.github.com>
2021-11-24 20:42:46 +0530

    Update AboutUs.js

---
+++ src/containers/AboutUs.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./AboutUs.css";
+function About() {
+  return (
+    <div className="About">
+      <div class="aboutContainer">
+        <h1>The Minds Foundation</h1>
+        <h3>
+          We are an NGO with the vision to create a world that fosters a shared
+          approach to mental wellness — in which anyone, anywhere can access the
+          resources they deserve.
+        </h3>
+        <img
+          src="https://images-workbench.99static.com/oCTVMxjO7JRDxACYX3Ht2eupLrI=/99designs-contests-attachments/115/115348/attachment_115348251"
+          alt="Minds Foundation Logo"
+        />
+        <h3>
+          During the COVID-19 pandemic, concerns about mental health and
+          substance use have grown, including concerns about suicidal ideation.
+          During these times, when maintaining your mental health has become so
+          important, talking to a professional Therapist or Consultant can help
+          people cope with a wide range of experiences.
+        </h3>
+        <h3>
+          We aim to improve the mental health of all people in India, to help
+          individuals lift themselves out of the impacts of poor mental health.
+          We seek to ensure that all people, especially those with the fewest
+          resources hold a good state of mental health to secure opportunities
+          to help themselves.
+        </h3>
+      </div>
+    </div>
+  );
+}
+export default About;
