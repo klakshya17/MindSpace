5909e9893b7ee08d56323cb2b25e1c24b90213bf
Kumar Lakshya <klakshya170@gmail.com>
2021-11-24 11:29:43 +0530

    added login and signup transitions

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,17 @@
 import React from "react";
 import "./Login.css";
 function Login() {
+  const signUpButton = document.getElementById("signUp");
+  const signInButton = document.getElementById("signIn");
+  const container = document.getElementById("container");
+
+  signUpButton.addEventListener("click", () => {
+    container.classList.add("right-panel-active");
+  });
+
+  signInButton.addEventListener("click", () => {
+    container.classList.remove("right-panel-active");
+  });
   return (
     <div className="Login">
       <div class="container" id="container">
