e130b2cb065583dd2f30cdde4240d36f8d976044
Devansh-pathak <69319667+Devansh-pathak@users.noreply.github.com>
2021-11-24 12:17:32 +0530

    Update Blogs.js

--- src/attributes/Blogs.js
+++ src/attributes/Blogs.js
@@ -35,7 +35,47 @@ A child’s risk of having bipolar disorder is 30% if one parent has bipolar dis
 In children, bipolar disorder is the 3rd highest reason for doctor visits, 2nd highest reason for hospital ER visits 32
 3.4 million children and adolescents suffer bipolar disorder 33",
     writer: "Michelle Clark",
-    image: "",
-  },
+    image: "", },
+  {
+    title: "Center for mental health",
+    date: "2nd November",
+    content: "Being gainfully employed has long been thought of as a key social determinant of health and wellbeing. In many ways ‘working’ goes beyond just earning an income – it provides a sense of worth, of purpose, of contributing to something bigger.
+
+However, the past 18 months or so, as we first responded to and then learned to live with Covid-19, have placed a new level of scrutiny on that assumption. Our experience has highlighted the importance of having ‘good work’ where terms and conditions focus on supporting individual employees’ health and wellbeing – but also support public health more generally.
+
+This is surely no way to build a resilient society, when those we rely on most in our time of need are valued the least
+
+Our Thursday night clapping for ‘key workers’ somehow rings hollow when we understand that employees in health and social care, in our supermarkets and the logistics sectors that kept us all going during the darker days of the pandemic, remain in the stubbornly low pay and insecure employment bracket.
+
+This is surely no way to build a resilient society, when those we rely on most in our time of need are valued the least and the stresses and anxiety placed on them only serve to further undermine our foundations.
+
+The Greater Manchester Good Employment Charter has been established with a clear agenda to set out what good employment should feel like, and to build a movement of good employment to address poor conditions and celebrate the best practice. We’ve defined ‘good work’ through seven key characteristics:
+
+The Real Living Wage
+Secure Work
+Flexible Work
+Recruitment
+Good Management
+Employee Engagement and Voice
+Health & Wellbeing.
+Of course, good employment practice starts before an employee joins an organisation – where employers should be open and transparent in their recruitment practices – embracing all the principles of equality, diversity and inclusion, ensuring that there are fair and equitable processes throughout. Indeed, the pursuit of equality through employment sets a leadership culture which will deliver good employment standards.
+
+Fair pay is fundamental to good employment, and the Charter specifies that all employees should be paid the Real Living Wage (as defined each year by the Resolution Foundation). This is the bare minimum, as we also recognise that other elements of remuneration such as sick pay provision (from day one) and living pensions are also fundamental to providing a baseline of financial security.
+
+The past 18 months have demonstrated that ‘life happens’, and that employees can thrive and be more productive when they take a more pragmatic approach
+
+Secure work is of equal importance. Understanding that your job is secured and protected by employment law, that your shift patterns are predictable and that your income is steady, provides the confidence to plan financially from week to week and month to month.
+
+The ability to be able to work flexibly from day one is also a hallmark of good work. The past 18 months have demonstrated that ‘life happens’, and that employees can thrive and be more productive when they take a more pragmatic approach to delivering their work at a time and place that works for them as well as the employer. Working rigid fixed hours at the same place each day now looks old and tired compared to a more blended and hybrid approach that allows employees to forge a work/life balance that brings better perspective and less stress.
+
+Ensuring that employees have a meaningful voice in the workplace is vital. Whether through Trade Union representation (where appropriate) or through other engagement mechanisms, good employers understand that involving, listening to and acting upon the concerns or ideas of their colleagues will enrich the organisational culture and bring genuine empowerment.
+
+good employers will place the wellbeing of their staff at the heart of what they do
+
+All of this requires excellent management practice, and the challenges presented to people management throughout the pandemic have been testing. This has allowed the Charter to focus and develop support for employers to help develop good people managers that understand how to be empathetic and that are keen to understand their staff better and help them grow, develop and progress in their working lives.
+
+All of this will help build a workplace that will help support health and wellbeing – but of course there is always more that can be done, and good employers will place the wellbeing of their staff at the heart of what they do. Gone are the days when a fruit bowl and a yoga class will tick the box of ‘looking after the health of your staff’. The relationship between work and health – and particularly mental wellbeing – is much deeper than that. This is why, in partnership with our colleagues at the GM Health and Social Care Partnership, we published our Mental health toolkit for employers as a resource for employers everywhere, to help them support their employees and understand the steps they can take to be an even better employer.",
+    writer: "Ian mcarthur",
+    image: "", }
 ];
 export default Blogs;
