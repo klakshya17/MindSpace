39524e62b51abbf75c6e807bd511b2bac8ee9d1c
Devansh-pathak <69319667+Devansh-pathak@users.noreply.github.com>
2021-11-24 11:50:45 +0530

    Update Doctors.js

--- src/attributes/Doctors.js
+++ src/attributes/Doctors.js
@@ -1,17 +1,18 @@
 export const Doctors = [
   {
     name: "Dr.Murphy",
-    degree: "MBBS",
-    about: "BA,MA-Pyschology-PHD-clinical pyschology
-             Consulatant-clinical pyschology
+    degree: "BA,MA-Pyschology-PHD-clinical pyschology",
+    about: " Consulatant-clinical pyschology
               Fortis Hirandani hospital,Vashi,Navi Mumbai
               Pyschologist
            Experience-18 years of practise",
   },
   {
-    name: "Dr.Murphy",
-    degree: "",
-    about: " ",
+    name: "Dr. Sonia Kapur",
+    degree: "BA-psychology MA-pyschology Phd-psychology",
+    about: "Consultant-psychology
+             Max Hospital,Delhi
+             pyschologist",
   },
   {
     name: "Dr.Murphy",
