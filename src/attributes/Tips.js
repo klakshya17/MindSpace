70d019319a8b0a49150afdca779afc130a67c10d
Devansh-pathak <69319667+Devansh-pathak@users.noreply.github.com>
2021-11-24 14:37:42 +0530

    Update Tips.js

--- src/attributes/Tips.js
+++ src/attributes/Tips.js
@@ -1,11 +1,27 @@
 export const Tips = [
   {
-    title: "Daily Tips",
-    content: "",
+    title: "Daily Tips 1(foods for mental health)",
+    content: "What to eat to improve your mental health?
+
+Leafy Greens. Those who consume their daily servings of leafy greens,such as spinach or kale,are said to have a slower rate of cognitive decline than those who ...
+Berries. Berries,such as raspberries,strawberries,and blackberries,are extremely rich in Vitamin C and many other antioxidants.
+Yogurt
+Walnuts
+Salmon",
   },
   {
-    title: "abc",
-    content: "",
+    title: "Daily tips 2(Treatments for mental health)",
+    content: "How to improve treatments for mental illness?
+
+Educate yourself. Before you even attempt to raise awareness of a particular mental illness or treatments for it,you need to educate yourself.
+Educate others. After you have educated yourself properly,it is important to spread the word about various conditions and the treatments (or lack thereof).
+Donations. ...
+Clinical trials. ...
+Biohacking. ...",
+  },
+     title: "Daily Tips 3",
+    content: "Eat Healthy:  Vegetables and fruits? Absolutely! Nutritious foods. Sure. Don't drink 10 cups of anything in a day, unless it's water. But healthy eating also means having a healthy attitude toward food. Enjoy meals with friends, try new foods and try not to obsess over food. If you do find that your relationship to food is affecting your mental or physical health, get the facts on eating disorders and take the important step of finding help. 
+Sleep Well:  The American Academy of Sleep Medicine recommends between 8–10 hours of sleep per night for teenagers and over 7 hours for those ages 20 and up. But sleeping well also refers to when you sleep and the quality of that sleep. Sleep health expert, Dr. Eleanor McGlinchey recommends that you wake up at the same time every day, even on weekends and holidays. This simple trick will help you fight that feeling of jet lag on Monday morning (also known as "social jet lag") and ensure that you are functioning and feeling your best. ",
   },
 ];
 export default Tips;
