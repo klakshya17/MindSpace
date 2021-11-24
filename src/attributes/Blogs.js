7caba5a2f03f0ba36b7c33fb556f8280009de670
Devansh-pathak <69319667+Devansh-pathak@users.noreply.github.com>
2021-11-24 12:09:56 +0530

    Update Blogs.js

--- src/attributes/Blogs.js
+++ src/attributes/Blogs.js
@@ -1,9 +1,42 @@
 export const Blogs = [
   {
-    title: "Health Blog",
+    title: "Bipolar Bandit",
     date: "24 oct ",
-    content: "",
-    writer: "Writer",
+    content: "Intresting Statistics about bipolar disorder
+               People with bipolar disorder, on average, suffer 10 years before receiving treatment  and only 49% of bipolar individuals receive treatment. 1
+The average age of American bipolar disorder onset is 25 years old  2
+83% of bipolar cases are considered severe. 3
+More than 66% of people with bipolar disorder have one or more relatives with bipolar disorder or clinical depression 4
+Bipolar individuals’ average bipolar episodes last 3 – 6 months 6
+The bipolar suicide rate is 60 times higher than that of the general public and one in five people with bipolar disorder commits suicide. 8,7
+Bipolar disorder is the 4th-highest reason for disability 35  AND 200,000 people with bipolar disorder are homeless 9
+69% of bipolar patients are mis-diagnosed at least 3.5 times 10
+10% of bipolar disorder patients have onset of symptoms in their 40s-50s  11
+People with bipolar disorder relapse rate 80% within 2 years if they don’t take medications; 40% with meds 12
+Except for side effects, 90% of bipolar patients are satisfied with their meds 13
+Mood disorder patients are more likely to be obese, smoke and have heart disease 14
+Adults who earned high or top school grades are 4X more likely to develop bipolar disorder than their peers 15
+80% of those with bipolar disorder have at least one bipolar family member 16
+Bipolar disorder affects males and females 18
+Following the first bipolar episode is a 75% chance of recurrence even if treatment plan is followed 20
+Bipolar disorder is more common in high achievers [than the general population 21
+People with bipolar disorder live 9.2 years less than the average 78 years 23
+50% of bipolar individuals abuse drugs and alcohol 24
+35% of people with bipolar disorder are obese and are 2X as likely to die of heart disease, diabetes, stroke. 25
+The treatment success rate for bipolar disorder is a remarkable 80 percent. 37
+World  Statistics:
+
+2.4% of the world population has bipolar disorder 27
+Less than 50% of the planet’s bipolar population receives any treatment 28
+More than 33% of world’s Bipolar Disorder people have substance abuse disorders 29
+Bipolar disorder is the 6th leading reason for disability worldwide 30
+CHILDREN:
+
+A child’s risk of having bipolar disorder is 30% if one parent has bipolar disorder, 75% if two parents suffer the disorder 31
+In children, bipolar disorder is the 3rd highest reason for doctor visits, 2nd highest reason for hospital ER visits 32
+3.4 million children and adolescents suffer bipolar disorder 33
+,
+    writer: "Michelle Clark",
   },
   {
     title: "abc",
